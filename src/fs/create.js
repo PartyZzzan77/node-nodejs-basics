29e452a9764db3e62c88eac960a07ccb83030b0f
Mitya <partyzan327@gmail.com>
2022-11-29 20:10:11 +0300

    feat: implement the create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
+import { access, constants } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const greeting = Buffer.from('I am fresh and young');
+  const path = './files/fresh.txt';
+  const err = 'FS operation failed';
+
+  try {
+    await access(path);
+    process.stdout.write(err + '\n');
+  } catch (error) {
+    await writeFile(path, greeting);
+  }
 };
 
 await create();
