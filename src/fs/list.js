7fbc795644ba157d5b87ae8b1f1872b63f94e324
Mitya <partyzan327@gmail.com>
2022-11-30 20:10:57 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir } from 'node:fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  const path = new URL('./files', import.meta.url);
+  const errMessage = 'FS operation failed';
+
+  try {
+    const files = await readdir(path);
+    for (const file of files) {
+      process.stdout.write(file + '\n');
+    }
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      throw new Error(errMessage);
+    }
+  }
 };
 
 await list();
