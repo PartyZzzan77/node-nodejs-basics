ef37dd96b2b5f55b86eb62af6255f99d4462ef88
Mitya <partyzan327@gmail.com>
2022-11-30 20:34:16 +0300

    refactor: throw error create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,17 +4,20 @@ import { access } from 'node:fs/promises';
 
 const create = async () => {
   const greeting = Buffer.from('I am fresh and young');
-  const path = './files/fresh.txt';
+  const path = new URL('./files/fresh.txt', import.meta.url);
   const errMessage = 'FS operation failed';
 
   try {
-    await access(path);
+    const existFile = await access(path);
+    if (!existFile) {
+      throw new Error(errMessage);
+    }
   } catch (error) {
     if (error.code === 'ENOENT') {
       await writeFile(path, greeting);
       return;
     }
-    throw new Error(errMessage);
+    process.stdout.write(error.toString() + '\n');
   }
 };
 
