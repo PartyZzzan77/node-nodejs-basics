b6377f469acb3f574550e44468e6894ab7a3deb0
Mitya <partyzan327@gmail.com>
2022-11-30 22:17:48 +0300

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+  const srcPath = new URL('./files', import.meta.url);
+  const dirPath = new URL('./files_copy', import.meta.url);
+  const errMessage = 'FS operation failed';
+
+  try {
+    if (!existsSync(srcPath) || existsSync(dirPath)) {
+      throw new Error(errMessage);
+    }
+    await cp(srcPath, dirPath, { recursive: true });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 copy();
