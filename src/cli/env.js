093e148815ae385eec47ef506677480566d61811
Mitya <partyzan327@gmail.com>
2022-11-30 23:24:41 +0300

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  const entries = Object.entries(process.env);
+  const withRS_ = entries.filter(([key, value]) => {
+    if (key.includes('RSS_')) {
+      return key;
+    }
+  });
+
+  const result = withRS_
+    .map(([key, value]) => `${key}name=value${value}`)
+    .join('; ');
+
+  process.stdout.write(result + '\n');
 };
 
 parseEnv();
