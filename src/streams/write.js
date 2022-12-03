812bf5bb6452b79360735bef4a6fe1a971ee4053
Mitya <partyzan327@gmail.com>
2022-12-03 21:26:10 +0300

    feat: implement streams-write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import fs from 'node:fs';
+import { pipeline } from 'stream/promises';
+
 const write = async () => {
-    // Write your code here 
+  const path = new URL('./files/fileToWrite.txt', import.meta.url);
+  const ws = fs.createWriteStream(path);
+
+  await pipeline(process.stdin, ws);
 };
 
 await write();
