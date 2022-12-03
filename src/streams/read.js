3071cfd10e5841d8376df710d3e06260d4ac4718
Mitya <partyzan327@gmail.com>
2022-12-03 21:19:59 +0300

    feat: implement streams-read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
+import fs from 'node:fs';
+import { pipeline } from 'stream/promises';
+
 const read = async () => {
-    // Write your code here 
+  const path = new URL('./files/fileToRead.txt', import.meta.url);
+  const rs = fs.createReadStream(path);
+
+  await pipeline(rs, process.stdout);
 };
 
 await read();
