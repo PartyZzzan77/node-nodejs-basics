1dbea637210d33796375446e0e76331eaee93c8f
Mitya <partyzan327@gmail.com>
2022-12-05 15:21:02 +0300

    feat: redistribute flows

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,7 +1,9 @@
 import { fork } from 'node:child_process';
 
 const spawnChildProcess = async (args) => {
-  fork('./files/script.js', args);
+  fork('./files/script.js', args, {
+    stdio: [0, 1, 2, 'ipc'],
+  });
 };
 
 spawnChildProcess();
