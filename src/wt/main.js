cba29fda17dd341d9f60f9cca266991f4ffe9ae0
Mitya <partyzan327@gmail.com>
2022-12-26 17:02:15 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+const getKernelCompute = (w, pid) => {
+    return new Promise((res, rej) => {
+        w.on('message', (data) => res({ status: 'resolved', data: data, pid }));
+        w.on('error', (err) =>
+            res({ status: err.message.split("'")[0].trim(), data: null, pid })
+        );
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    let counter = 10;
+    let result = [];
+
+    for await (const _ of os.cpus()) {
+        counter++;
+        const worker = new Worker('./worker.js', { workerData: counter });
+        result.push(await getKernelCompute(worker, counter - 1));
+    }
+
+    console.dir(result);
 };
 
 await performCalculations();
